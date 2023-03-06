a0342b6ae0a5f680801c863aa47712866b612340
Vincent550102 <5020559@gmail.com>
2023-03-06 03:44:56 +0000

    fix type

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ sudo docker compose up -d
 ```
 [指令列表]
 /help 印出這個列表
-/addchain <id> [id]... 增加想要連鎖發話的 id，可透過 /mygroupid 來獲得 id
+/addchain <id> [id]... 增加想要連鎖發話的 id，可透過 /id 來獲得 id
 /listchain 列出當前有設定哪些連鎖發話
 /deletechain <id> 刪除指定連鎖發話
 /wall <message> 連鎖發話！
--- app.py
+++ app.py
@@ -13,7 +13,7 @@ class TelegramBot():
     async def help(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         await context.bot.send_message(chat_id=update.effective_chat.id, text="""[指令列表]
 /help 印出這個列表
-/addchain <id> [id]... 增加想要連鎖發話的 id，可透過 /mygroupid 來獲得 id
+/addchain <id> [id]... 增加想要連鎖發話的 id，可透過 /id 來獲得 id
 /listchain 列出當前有設定哪些連鎖發話
 /deletechain <id> 刪除指定連鎖發話
 /wall <message> 連鎖發話！
